4b1516e130cfbfa19ee3bf1cc85a09527611115f
shun0918 <nihei@tebiki.co.jp>
2022-12-09 23:40:12 +0900

    chore: Add octpat

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-console.log('hello!');
+import { Octokit } from "octokit";
 
 console.log('Target repository is ' + process.env.GITHUB_TARGET_REPOSITORY || 'not set');
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_ACCESS_TOKEN
+});
+
+const [owner, repo] = process.env.GITHUB_TARGET_REPOSITORY.split('/');
+
+octokit.request("GET /repos/{owner}/{repo}/issues", {
+  owner,
+  repo,
+}).then((data) => {
+  console.log('success');
+  console.log(data);
+}).catch((error) => {
+  console.log('error');
+  console.log(error);
+});;
