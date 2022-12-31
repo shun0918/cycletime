aaf670f490b8d521e039e52d6dfb2187cc981c14
shun0918 <nihei@tebiki.co.jp>
2022-12-31 18:52:46 +0900

    feat: Enable CLI

---
+++ bin/cycletime.ts
@@ -0,0 +1,3 @@
+import { main } from './../index';
+
+main(parseInt(process.argv[2]) || 10, parseInt(process.argv[3]) || 1);
--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
 import { showTable } from './presentation.js';
 import { pullCycleTimes } from './services/pull-cycle-times.js';
 
-const main = async (perPage: number, page: number) => {
+export const main = async (perPage: number, page: number) => {
   const res = await pullCycleTimes(perPage, page);
   showTable(res);
 }
-
-main(parseInt(process.argv[2]) || 10, parseInt(process.argv[3]) || 1);
